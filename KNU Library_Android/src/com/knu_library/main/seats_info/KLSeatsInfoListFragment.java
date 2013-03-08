5c8a2554d83bb90fe86c0eca3e011ea3c3975e01
Steven Kim <kwosu87@me.com>
2013-03-08 14:20:19 +0900

    URL Connection 완성. 파싱만 완료하면 됨

--- KNU Library_Android/src/com/knu_library/main/seats_info/KLSeatsInfoListFragment.java
+++ KNU Library_Android/src/com/knu_library/main/seats_info/KLSeatsInfoListFragment.java
@@ -2,17 +2,31 @@ package com.knu_library.main.seats_info;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.widget.ListAdapter;
 
 public class KLSeatsInfoListFragment extends ListFragment {
 
+	private static final String TAG = "KLSeatsInfoListFragment";
+	
+	public Context m_context;
+	
 	@Override 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		
 		init();
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		
+		m_context = activity;
 	}
 	
 	public void init()
@@ -24,11 +38,17 @@ public class KLSeatsInfoListFragment extends ListFragment {
 	
 	public void threadDidParsing(ArrayList<KLSeatInfoModel> seatInfoModelList) {
 		// 필요한 정보를 받아서 파싱을 완료하고 어댑터를 만들어서 보여줌. 그동안은 로딩 화면을 보여줄 수 있으면 좋을 듯.
-		ListAdapter roomsListAdapter = new KLSeatsInfoListAdapter(getActivity());
-		setListAdapter(roomsListAdapter);
+		((Activity)m_context).runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				ListAdapter roomsListAdapter = new KLSeatsInfoListAdapter(getActivity());
+				setListAdapter(roomsListAdapter);	
+			}
+		});
 	}
 	
 	public void threadFailedParsing() {
-		
+		Log.i(TAG, "threadFailedParsing");
 	}
 }
--- KNU Library_Android/src/com/knu_library/main/seats_info/KLSeatsInfoParsingThread.java
+++ KNU Library_Android/src/com/knu_library/main/seats_info/KLSeatsInfoParsingThread.java
@@ -1,8 +1,14 @@
 package com.knu_library.main.seats_info;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.util.Log;
 
 /**
  * 경북대 도서관 서버에서 자리 정보를 얻어와서 파싱을 하고, 원하는 자료 구조로 정리 
@@ -11,6 +17,8 @@ import android.content.Context;
  */
 public class KLSeatsInfoParsingThread extends Thread {
 
+	private static final String TAG = "KLSeatsInfoParsingThread";
+	
 	Context m_context = null;
 	KLSeatsInfoListFragment m_SuperListFragment = null;
 	
@@ -27,8 +35,39 @@ public class KLSeatsInfoParsingThread extends Thread {
 		ArrayList<KLSeatInfoModel> seatsInfoModelList = new ArrayList<KLSeatInfoModel>();
 		
 		// 파싱 시작
-		
-		// 모든 처리가 완료되면 ListFragment에 콜백
-		m_SuperListFragment.threadDidParsing(seatsInfoModelList);
+		try {
+			URL url = new URL("http://libseat.knu.ac.kr/domian5.asp");
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			// DoInput 은 GET 방식 DoOutput 은 POST 방식
+			connection.setDoInput(true);
+			
+			StringBuilder htmlString = new StringBuilder();
+			if (connection != null) {
+				connection.setConnectTimeout(10000);
+				connection.setUseCaches(false);
+				if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+					
+					BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "EUC-KR"));
+					for(;;) {
+						String line = br.readLine();
+						if (line == null) {
+							break;
+						}
+						htmlString.append(line + "\n");
+					}
+					br.close();
+				}
+				connection.disconnect();
+			}
+
+			// 모든 처리가 완료되면 ListFragment에 콜백
+			Log.i(TAG, htmlString.toString());
+			m_SuperListFragment.threadDidParsing(seatsInfoModelList);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			// 실패 했을 경우를 대비하기
+			m_SuperListFragment.threadFailedParsing();
+		}
 	}
 }
